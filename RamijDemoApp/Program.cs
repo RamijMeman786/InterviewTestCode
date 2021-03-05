c85f83f5661ced25d6533d8c8d4c236a45216fab
RamijMeman786 <rameejmemon786@gmail.com>
2021-03-05 09:54:26 +0530

    Add Project git

---
+++ RamijDemoApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RamijDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Inputstr;
+
+            int MaxCount = 0;
+
+            string FrequentCharacter = "";
+            string resultCharacter = "";
+
+
+            Console.Write("Input the string : ");
+
+            Inputstr = Console.ReadLine();
+
+            Console.Write("The string you entered is : {0}\n", Inputstr);
+
+
+            for (int i = 0; i < Inputstr.Length; i++)
+            {
+                int Count = 0;
+
+                for (int j = 0; j < Inputstr.Length; j++)
+                {
+                    if (char.ToUpperInvariant(Inputstr[i]) == char.ToUpperInvariant(Inputstr[j]))
+                    {
+                        FrequentCharacter = Convert.ToString(Inputstr[i]);
+                        Count++;
+                    }
+                }
+                if (Count > MaxCount)
+                {
+                    resultCharacter = FrequentCharacter;
+                    MaxCount = Count;
+                }
+            }
+
+            Console.Write("Frequent Character:" + resultCharacter);
+
+            Console.Write("\n");
+
+            Console.Write("Count:" + MaxCount);
+
+            Console.ReadLine();
+        }
+    }
+}
